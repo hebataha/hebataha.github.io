8e50568a02695e98d21ff6c0ecd6449d29c3db8c
heba <amltaha866@yahoo.com>
2020-01-16 18:53:42 +0200

    first commit

---
+++ precache-manifest.85010443ebff7f127c7c664a216c582b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceceae357d266e939b319bf96184d26b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aaf1ba58e51f0ac7dc7",
+    "url": "/static/css/main.b8fce3cc.chunk.css"
+  },
+  {
+    "revision": "0aeb1f507aeb20cf28d7",
+    "url": "/static/js/2.c61c7936.chunk.js"
+  },
+  {
+    "revision": "ba1a0ff350f8b78aab2136f45a725a19",
+    "url": "/static/js/2.c61c7936.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7aaf1ba58e51f0ac7dc7",
+    "url": "/static/js/main.ecd7cfab.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  },
+  {
+    "revision": "e0b1fa39c0c8282eb0615c4b931f718e",
+    "url": "/static/media/Subtraction.e0b1fa39.svg"
+  },
+  {
+    "revision": "cd4b936fc6247717ac87844f223eeb24",
+    "url": "/static/media/ad.cd4b936f.png"
+  },
+  {
+    "revision": "b5e0c8b86d83432c19ef09aca6668c22",
+    "url": "/static/media/all.b5e0c8b8.svg"
+  },
+  {
+    "revision": "626f41f3f6a1bbfbb5bf1de836301176",
+    "url": "/static/media/facebook.626f41f3.svg"
+  },
+  {
+    "revision": "0397a71e7ff96c6e8ac840914a962653",
+    "url": "/static/media/forest.0397a71e.jpeg"
+  },
+  {
+    "revision": "68685c424937bb106f94e8305e05f7e2",
+    "url": "/static/media/instagram.68685c42.svg"
+  },
+  {
+    "revision": "948c5dbfeb68091b24a7da756c0945fb",
+    "url": "/static/media/left.948c5dbf.png"
+  },
+  {
+    "revision": "bca21e56422c3e757642fffe159a2633",
+    "url": "/static/media/p.bca21e56.jpeg"
+  },
+  {
+    "revision": "0f9c6b30cba13e3539d0ab4517da2598",
+    "url": "/static/media/pinterest.0f9c6b30.svg"
+  },
+  {
+    "revision": "831f65f22fa87a170173077942865936",
+    "url": "/static/media/pp.831f65f2.jpeg"
+  },
+  {
+    "revision": "cb81e21ef786a7aa6c571748e84f0521",
+    "url": "/static/media/re.cb81e21e.png"
+  },
+  {
+    "revision": "b24664ab86f3830b8b5e6a7b5fa00e04",
+    "url": "/static/media/slide.b24664ab.png"
+  },
+  {
+    "revision": "6900fa705b2998d1498e72a628ca8427",
+    "url": "/static/media/twitter.6900fa70.svg"
+  },
+  {
+    "revision": "e3a20c0a52733d1dbbf6f936ef4626cc",
+    "url": "/static/media/user.e3a20c0a.svg"
+  }
+]);
